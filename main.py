2a176121d49c7bffbd0082a0ccde5b08e87e23b7
Luiz <luizrodrigo46@hotmail.com>
2017-07-16 14:45:19 -0300

    refactored the method to capture all publications

--- main.py
+++ main.py
@@ -11,9 +11,8 @@ password = config('FACEBOOK_PASSWORD')
 driver = webdriver.Firefox()
 
 driver.get("http://m.facebook.com/")
-time.sleep(3)
-
 print("Connected with facebook")
+time.sleep(2)
 
 email_field = driver.find_element_by_name('email')
 email_field.clear()
@@ -27,18 +26,17 @@ print("Password inserted")
     
 login_button = driver.find_element_by_name('login')
 login_button.click()
-print("Button login clicked")
-    
-time.sleep(4)
-    
-print("Entry on touch")
-
+print("Button login clicked.")
+time.sleep(2)
 
 ok_button = driver.find_element_by_xpath('//input[@value="OK"]').click()
-time.sleep(4)
+print("Ok pressed in Entry on touch page.")
+time.sleep(2)
     
 perfil = driver.find_element_by_link_text('Perfil').click()
-time.sleep(4)
+print("Entry on Perfil.")
+time.sleep(2)
+
 year = driver.find_element_by_link_text('2017').click()
 time.sleep(4)
 
@@ -48,25 +46,26 @@ while more:
     pubs = BeautifulSoup(driver.page_source, 'html.parser')
     try:
         main_window = driver.current_window_handle
-
-        for pub in pubs.find_all(class_='bp cd ce'):
-            reaction_link = pub.find('a', text='Curtir').find_previous_sibling('a').get('href')
         
+        for pub in pubs.find_all('a', text='Curtir'):
+            reaction_link = pub.find_previous_sibling('a').get('href')
+            print(reaction_link)
+
             driver.execute_script('window.open("{0}");'.format(reaction_link))
             time.sleep(4)
             driver.switch_to_window(driver.window_handles[1])
             time.sleep(4)
             driver.find_element_by_xpath('//div[@id="add_comment_switcher_placeholder"]/following-sibling::div/a').click()
             time.sleep(4)
-            #driver.find_element_by_tag_name('div').send_keys(Keys.CONTROL+"w")
             driver.switch_to_window(main_window)
     except:
+        time.sleep(4)
         #the publication does have any reactions
         pass
     try:
         more = driver.find_element_by_link_text('Mostrar mais')
         more.click()
-        time.sleep(4)
+        time.sleep(2)
     except:
         more = 0
 driver.close() 
