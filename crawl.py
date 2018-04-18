bea07b8468cad3b23cc6cbad9e17de2102a1569a
Luiz <luizrodrigo46@hotmail.com>
2018-04-18 08:02:55 -0300

    PEP-8 updates

--- crawl.py
+++ crawl.py
@@ -2,19 +2,20 @@ from decouple import config
 from pymongo import MongoClient
 from spider.spider import FacebookSpider
 
+
 def start_crawl():
-	conn = MongoClient('localhost', 27017)
+    conn = MongoClient('localhost', 27017)
 
-	db = conn['facebook_reactions']
+    db = conn['facebook_reactions']
 
-	timeline = db['timeline']
+    timeline = db['timeline']
 
-	facebook_email = config('FACEBOOK_EMAIL', default=False) 
-	facebook_password = config('FACEBOOK_PASSWORD', default=False)
+    facebook_email = config('FACEBOOK_EMAIL', default=False) 
+    facebook_password = config('FACEBOOK_PASSWORD', default=False)
 
-	spider = FacebookSpider(timeline)
-	spider.login(facebook_email, facebook_password)
-	spider.crawl()
+    spider = FacebookSpider(timeline)
+    spider.login(facebook_email, facebook_password)
+    spider.crawl()
 
 
 if __name__ == '__main__':
